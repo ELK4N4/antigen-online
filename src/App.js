fc44f7a1277b953611b3b758d6fae9d9a2735499
ELK4N4 <elkana546@gmail.com>
2022-01-09 23:35:01 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ReactiveButton from 'reactive-button';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [status, setStatus] = useState('idle');
+  const [buttonText, setButtonText] = useState('בדוק');
+  const resultsOptions = ['חיובי', 'שלילי'];
+  const resultsColors = ['red', 'green']
+  const [resultsIndex, setResultsIndex] = useState();
+  const [color, setColor] = useState('teal');
+
+   const onClickHandler = () => {
+    setStatus('loading');
+      setTimeout(() => {
+        const index = Math.floor(Math.random() * 2);
+        setResultsIndex(index)
+        setButtonText(resultsOptions[index])
+        setStatus('success');
+        setColor(resultsColors[index])
+    }, 3000);
+   }
+
+   return (
+    <div style={{height: '100%', weight: '100%', padding: '10%'}}>
+      <div className='container'>
+        <br />
+        <h1>
+          בדיקת אנטיגן אונליין
+        </h1>
+        <h2>
+        מהיר! ללא עלות! 50% יעילות!
+        </h2>
+        <br />
+        <br />
+        <ReactiveButton
+          buttonState={status}
+          onClick={onClickHandler}
+          color={color}
+          idleText={buttonText}
+          loadingText={'מבצע בדיקה'}
+          successText={resultsOptions[resultsIndex]}
+          errorText={'Error'}
+          type={'button'}
+          className={'class1 class2'}
+          style={{fontWeight: 'bold', fontSize: 20, borderRadius: '5px' }}
+          shadow={false}
+          rounded
+          size={'large'}
+          block={false}
+          messageDuration={2000}
+          disabled={false}
+          buttonRef={null}
+          width={'70%'}
+          height={50}
+          animation={true}
+        />
+        <br />
+        <h4>
+          האתר נבנה בהומור המציאות והנתונים שהאתר המציג אינם אמיתיים ואינם מהווים שום אישור רפואי
+        </h4>
+        <br />
+      </div>
     </div>
   );
 }
